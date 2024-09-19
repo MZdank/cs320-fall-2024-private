b9f7d16c24eb347dd2a258c7a487b5dfe641a7b2
MZdank <MZdank@bu.edu>
2024-09-19 12:06:48 -0400

    Think it works now?

--- assigns/assign02/lib/assign02_03.ml
+++ assigns/assign02/lib/assign02_03.ml
@@ -30,18 +30,23 @@ let drop n l =
   aux 0 l
 
 let rec make_move path updown leftright =
+  (*if path is empty we're at the end of the path*)
   match path with
   | [] -> (updown, leftright)
   | _ ->
+    (*Take the first element in path, create newpath without that element*)
     let move = take 1 path in
-    let _ = drop 1 path in
+    let newpath = drop 1 path in
+    (*move is either up or down, left or rith*)
     match move with
-      | [North] -> make_move path (updown + 1) leftright
-      | [South] -> make_move path (updown - 1) leftright
-      | [East] -> make_move path updown (leftright + 1)
-      | [West] -> make_move path updown (leftright - 1)
-      | _ -> (updown, leftright)
+      | [North] -> make_move newpath (updown + 1) leftright
+      | [South] -> make_move newpath (updown - 1) leftright
+      | [East] -> make_move newpath updown (leftright + 1)
+      | [West] -> make_move newpath updown (leftright - 1)
+      | _ -> (updown, leftright) (*I don't think I need this
+      since first match should check if move empty*)
 
 let dist path =
   let (updown, leftright) = make_move path 0 0 in
+  (*A^2 + B^2 = C^2*)
   sqrt (float_of_int ((updown) * (updown) + (leftright) * (leftright)))
