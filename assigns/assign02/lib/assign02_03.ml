c2eb0e1e06fef0c78bd73506be4d906bf3736265
MZdank <MZdank@bu.edu>
2024-09-19 10:36:32 -0400

    Think I'm on right track, syntax errors

---
+++ assigns/assign02/lib/assign02_03.ml
@@ -0,0 +1,44 @@
+type dir =
+  | North
+  | South
+  | East
+  | West 
+
+type path = dir list
+
+(*let is_close f1 f2 = abs_float (f1 -. f2) <.000001   *)
+
+(*let _ = assert (is_close (dist [North; North; South; East]) (sqrt 2.)) *)
+
+(*This is stolen from the class lib*)
+let take n l =
+  let[@tail_mod_cons] rec aux n l =
+    match n, l with
+    | 0, _ | _, [] -> []
+    | n, x::l -> x::aux (n - 1) l
+  in
+  (*if n < 0 then invalid_arg "List.take";*)
+  aux n l
+
+(*This is also stolen from the class lib*)
+let drop n l =
+  let rec aux i = function
+    | _x::l when i < n -> aux (i + 1) l
+    | rest -> rest
+  in
+  (*if n < 0 then invalid_arg "List.drop";*)
+  aux 0 l
+
+let dist path =
+  let rec make_move path updown leftright =
+    let move = take 1 path in
+    drop 1 path in
+    match move with
+      | [] -> (updown, leftright) 
+      | [North] -> make_move path (updown + 1) leftright
+      | [South] -> make_move path (updown - 1) leftright
+      | [East] -> make_move path updown (leftright + 1)
+      | [West] -> make_move path updown (leftright - 1)
+  in
+  let (updown, leftright) = make_move path 0 0
+  in sqrt (float_of_int ((updown) * (updown) + (leftright) * (leftright)))
